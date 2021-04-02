f3369c53803d9a3e9bff82b3488f4ddf55baa7f6
Mawers <54773772+Mawers@users.noreply.github.com>
2021-04-02 16:59:58 +0200

    segregacja kodu

--- js/cursor.js
+++ js/cursor.js
@@ -10,14 +10,3 @@ window.addEventListener('scroll', () => {
     cursor.style.top = scrollY + fromTop + 'px';
 })
 
-function zegar() {
-    var data = new Date();
-    var godzina = data.getHours();
-    var min = data.getMinutes();
-    var sek = data.getSeconds();
-     var terazjest = ""+godzina+
-    ((min<10)?":0":":")+min+
-    ((sek<10)?":0":":")+sek;
-    document.getElementById("zegarek").innerHTML = terazjest;
-    setTimeout("zegar()", 1000); }
-    zegar();
---
+++ js/zegar.js
@@ -0,0 +1,10 @@
+function zegar() {
+    var data = new Date();
+    var godzina = data.getHours();
+    var min = data.getMinutes();
+    var sek = data.getSeconds();
+    var terazjest = ""+godzina+((min<10)?":0":":")+min+((sek<10)?":0":":")+sek;
+    document.getElementById("zegarek").innerHTML = terazjest;
+    setTimeout("zegar()", 1000); 
+}
+    zegar();
