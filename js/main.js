4913456664be98e5728175043f2e92e75fbe75b2
Mawers <54773772+Mawers@users.noreply.github.com>
2021-04-02 16:36:38 +0200

    dodanie powitania i ulatwienie dodania cytatow do strony

--- js/main.js
+++ js/main.js
@@ -9,3 +9,15 @@ window.addEventListener('scroll', () => {
     const fromTop = parseInt(cursor.getAttribute('data-fromTop'));
     cursor.style.top = scrollY + fromTop + 'px';
 })
+
+function zegar() {
+    var data = new Date();
+    var godzina = data.getHours();
+    var min = data.getMinutes();
+    var sek = data.getSeconds();
+     var terazjest = ""+godzina+
+    ((min<10)?":0":":")+min+
+    ((sek<10)?":0":":")+sek;
+    document.getElementById("zegarek").innerHTML = terazjest;
+    setTimeout("zegar()", 1000); }
+    zegar();
